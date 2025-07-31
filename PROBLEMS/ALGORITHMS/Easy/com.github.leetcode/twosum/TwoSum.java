e3ac3200049f0727c1765690f1bd1d2087e3cecc
maris <marisk@taltech.ee>
2025-07-31 18:44:06 +0300

    Algorithm excersizes

---
+++ PROBLEMS/ALGORITHMS/Easy/com.github.leetcode/twosum/TwoSum.java
@@ -0,0 +1,23 @@
+package twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum  {
+    public int[] twoSum(int[] nums, int target) {
+        // for index mapping
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // Calculate what number is needed
+            int otherNum = target - nums[i];
+
+            if (numToIndex.containsKey(otherNum)) {
+                // Found the pair! Return indices
+                return new int[]{numToIndex.get(otherNum), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
